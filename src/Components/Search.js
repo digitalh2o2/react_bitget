779de53a23764760f44294e3f8aee3e3c398a01b
Saul <digitalh2o@gmail.com>
2018-01-15 21:27:04 -0600

    User input updates state and calls API successfully. Will extract logic and start populating results next

---
+++ src/Components/Search.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Search extends React.Component {
+  render() {
+    const { onSubmit, value, onChange, children } = this.props;
+    return (
+      <section>
+        <form onSubmit={onSubmit} className="input-field">
+          <div className="field has-addons">
+            <div className="control ">
+              <input
+                type="text"
+                className="input"
+                value={value}
+                onChange={onChange}
+              />
+            </div>
+            <div className="control">
+              <a className="button is-info">{children}</a>
+            </div>
+          </div>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Search;
--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,35 +1,84 @@
 import React from "react";
+import Search from "./Search";
 
-var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+var proxyUrl = "https://cors-anywhere.herokuapp.com/";
 const DEFAULT_QUERY = "megaman";
 
 class Test extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchTerm: ""
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  componentWillMount() {
-    fetch(proxyUrl + "https://api-2445582011268.apicast.io/games/?search=Halo", {
-      method: "GET",
-      headers: {
-        'user-key': "6e6712c1c94feb2bc74251324d8184db",
-        Accept: "application/json"
+  onSearch(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  onSubmit(e) {
+    const { searchTerm } = this.state;
+    this.setState({ searchTerm: searchTerm });
+    fetch(
+      proxyUrl +
+        `https://api-2445582011268.apicast.io/games/?search=${searchTerm}&fields=name,cover`,
+      {
+        method: "GET",
+        headers: {
+          "user-key": "6e6712c1c94feb2bc74251324d8184db",
+          Accept: "application/json"
+        }
       }
-    })
+    )
       .then(res => res.json())
       .then(resData => {
         console.log(resData);
       })
-      .catch((err) => {
+      .catch(err => {
+        console.log(err);
+      });
+    e.preventDefault();
+  }
+
+  componentWillMount() {
+    fetch(
+      proxyUrl +
+        `https://api-2445582011268.apicast.io/games/?search=${DEFAULT_QUERY}&fields=name,cover`,
+      {
+        method: "GET",
+        headers: {
+          "user-key": "6e6712c1c94feb2bc74251324d8184db",
+          Accept: "application/json"
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
         console.log(err);
       });
   }
 
   render() {
+    const { searchTerm } = this.props;
     return (
-      <div>
-        <p>Hi</p>
-      </div>
+      <section>
+        <div className="container">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearch}
+            onSubmit={this.onSubmit}
+          >
+            Search
+          </Search>
+        </div>
+      </section>
     );
   }
 }
