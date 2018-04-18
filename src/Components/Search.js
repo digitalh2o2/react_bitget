098c09cdfe78b683253612bc3da4fd420baeff57
digitalh2o <digitalh2o@gmail.com>
2018-04-17 23:12:51 -0500

    Filter asc or desc from main page results

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,31 +1,42 @@
 import React from "react";
 
 class Search extends React.Component {
-  render() {
-    const { onSubmit, value, onChange, children } = this.props;
-    return (
-      <section>
-        <form onSubmit={onSubmit} className="input-field">
-          <div className="field has-addons">
-            <div className="control ">
-              <input
-                type="text"
-                className="input"
-                value={value}
-                onChange={onChange}
-                placeholder="Search For A Game"
-              />
-            </div>
-            <div className="control">
-              <a className="button is-info" onClick={onSubmit}>
-                {children}
-              </a>
-            </div>
-          </div>
-        </form>
-      </section>
-    );
-  }
+	render() {
+		const { onSubmit, value, onChange, toggleSort, children } = this.props;
+		return (
+			<section>
+				<form onSubmit={onSubmit} className="input-field">
+					<div className="field has-addons">
+						<div className="control ">
+							<input
+								type="text"
+								className="input"
+								value={value}
+								onChange={onChange}
+								placeholder="Search For A Game"
+							/>
+						</div>
+						<div className="control">
+							<a className="button is-info" onClick={onSubmit}>
+								{children}
+							</a>
+							<select
+								className="button is-primary"
+								onClick={e => toggleSort(e)}
+								style={{ width: "100px" }}
+							>
+								<option value="" disabled selected hidden>
+									Filter..
+								</option>
+								<option value="a">A..Z</option>
+								<option value="z">Z..A</option>
+							</select>
+						</div>
+					</div>
+				</form>
+			</section>
+		);
+	}
 }
 
 export default Search;
